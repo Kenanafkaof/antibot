7d516bf6f619b694c751eb20fac7d11e840f0cc4
Kenanafkaof <70501296+Kenanafkaof@users.noreply.github.com>
2022-10-25 17:04:35 -0700

    check fingerprint

--- api.js
+++ api.js
@@ -37,6 +37,12 @@ const limiter = rateLimit({
 
 const initialCheck = function (req, res, next) {
     const userAgent = req.get('user-agent')
+    const hash_check = req.fingerprint.hash
+    if (hash_check == undefined || hash_check == "undefined" || hash_check == "" || hash_check == null) {
+        return res.status(403).json({
+            error: 'You are a bot!'
+        })
+    }
     const agents_blocked = ['python-requests', 'go-requests', 'requests', "360Spider","403checker","403enemy","80legs","Abonti","Aboundex","Aboundexbot","Acunetix","ADmantX","AfD-Verbotsverfahren","AhrefsBot","AIBOT","AiHitBot","Aipbot","Alexibot","Alligator","AllSubmitter","AlphaBot","Anarchie","Apexoo"];
     const found = agents_blocked.find(v => userAgent.includes(v));
     if (found) {
